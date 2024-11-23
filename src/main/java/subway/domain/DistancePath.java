d275e064437d54a3816b2e4e91c8e8b06f03e065
minSsan <msp1125@hanyang.ac.kr>
2024-11-23 17:38:57 +0900

    feat(domain): 다익스트라 경로 반환 도메인 서비스 추가

---
+++ src/main/java/subway/domain/DistancePath.java
@@ -0,0 +1,30 @@
+package subway.domain;
+
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import subway.domain.vo.Edge;
+import subway.domain.vo.Path;
+import subway.domain.vo.Station;
+
+import java.util.List;
+
+public class DistancePath {
+    WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+    public DistancePath(List<Station> stations, List<Edge> edges) {
+        for (Station station : stations) {
+            graph.addVertex(station.getName());
+        }
+        for (Edge edge : edges) {
+            graph.setEdgeWeight(graph.addEdge(edge.station1().getName(), edge.station2().getName()), edge.distance());
+        }
+    }
+
+    public Path getPath(String station1, String station2) {
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> stringPath = dijkstraShortestPath.getPath(station1, station2).getVertexList();
+        List<Station> path = stringPath.stream().map(Station::new).toList();
+        return new Path(path);
+    }
+}
