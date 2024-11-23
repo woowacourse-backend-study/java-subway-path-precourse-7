715556f3be9ae8b9d86c7f9df634732a17a15faf
권장순 <jsoonworld@gmail.com>
2024-11-23 17:13:39 +0900

    feat(Main) : 전체 흐름 기능 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -6,8 +6,13 @@ import subway.dto.DepartureStationInput;
 import subway.dto.MainFunctionInput;
 import subway.repository.LineRepository;
 import subway.repository.StationRepository;
+import subway.service.DistanceGraphService;
+import subway.service.TimeGraphService;
 import subway.view.InputView;
 import subway.view.OutputView;
+
+import java.util.List;
+import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
@@ -15,9 +20,7 @@ public class Application {
         LineRepository.initializeLines();
 
         OutputView outputView = new OutputView();
-
         outputView.printMainPrompt();
-
         outputView.printUserFunctionInputPrompt();
 
         InputView inputView = new InputView();
@@ -29,8 +32,38 @@ public class Application {
 
         outputView.printDepartureStationInputPrompt();
         DepartureStationInput departureStationInput = inputView.readDepartureStationInput();
+        String start = departureStationInput.getInput();
 
         outputView.printArrivalStationInputPrompt();
         ArrivalStationInput arrivalStationInput = inputView.readArrivalStationInput();
+        String end = arrivalStationInput.getInput();
+
+        if (start.equals(end)) {
+            throw new IllegalArgumentException("[ERROR] 출발역과 도착역이 동일합니다.");
+        }
+
+        if (Objects.equals(criteriaFunctionInput.getInput(), "1")) {
+            DistanceGraphService distanceGraphService = new DistanceGraphService();
+
+            List<String> shortestPath = distanceGraphService.getShortestPath(start, end);
+            double shortestDistance = distanceGraphService.getShortestDistance(start, end);
+            double totalTime = distanceGraphService.getTotalTime(start, end);
+
+            outputView.printResultPrompt(shortestDistance, totalTime);
+
+            outputView.printRouteResultPrompt(shortestPath);
+        }
+
+        if (Objects.equals(criteriaFunctionInput.getInput(), "2")) {
+            TimeGraphService timeGraphService = new TimeGraphService();
+
+            List<String> fastestPath = timeGraphService.getFastestPath(start, end);
+            double fastestTime = timeGraphService.getFastestTime(start, end);
+            double totalDistance = timeGraphService.getTotalDistance(start, end);
+
+            outputView.printResultPrompt(totalDistance, fastestTime);
+
+            outputView.printRouteResultPrompt(fastestPath);
+        }
     }
 }
