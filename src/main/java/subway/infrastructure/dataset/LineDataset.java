0fc73fbb10f2af1cf7c601e93d41aeb07c1b239e
minSsan <msp1125@hanyang.ac.kr>
2024-11-28 15:02:52 +0900

    feat(LineDataset): 노선 데이터셋 enum 클래스 추가

---
+++ src/main/java/subway/infrastructure/dataset/LineDataset.java
@@ -0,0 +1,40 @@
+package subway.infrastructure.dataset;
+
+import subway.domain.Edge;
+import subway.domain.Line;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum LineDataset {
+    LINE2(new Line("2호선", List.of(
+            Edge.from("교대역", "강남역", 2, 3),
+            Edge.from("강남역", "역삼역", 2, 3)
+    ))),
+    LINE3(new Line("3호선", List.of(
+            Edge.from("교대역", "남부터미널역", 3, 2),
+            Edge.from("남부터미널역", "양재역", 6, 5),
+            Edge.from("양재역", "매봉역", 1, 1)
+    ))),
+    LINE신분당(new Line("신분당선", List.of(
+            Edge.from("강남역", "양재역", 2, 8),
+            Edge.from("양재역", "양재시민의숲역", 10, 3)
+    )))
+    ;
+
+    private final Line line;
+
+    private LineDataset(Line line) {
+        this.line = line;
+    }
+
+    public Line getData() {
+        return line;
+    }
+
+    public List<Line> getLines() {
+        return Arrays.stream(values())
+                .map(LineDataset::getData)
+                .toList();
+    }
+}
