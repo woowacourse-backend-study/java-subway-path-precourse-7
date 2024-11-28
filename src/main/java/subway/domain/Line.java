5159bc2596156cfa6524ccfa30b2dc46ca9ac5f4
minSsan <msp1125@hanyang.ac.kr>
2024-11-28 15:02:15 +0900

    feat(Line): 노선 클래스 - 간선 필드 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,10 +1,15 @@
 package subway.domain;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Edge> edges;
 
-    public Line(String name) {
+    public Line(String name, List<Edge> edges) {
         this.name = name;
+        this.edges = edges;
     }
 
     public String getName() {
@@ -12,4 +17,11 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public List<Edge> getEdges() {
+        return Collections.unmodifiableList(edges);
+    }
+
+    public void addEdge(Edge edge) {
+        edges.add(edge);
+    }
 }
